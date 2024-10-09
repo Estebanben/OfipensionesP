bc7db260d2a85a62e12a5cd9533d0632cc8be4e1
Esteban Benavides <estebanben717@gmail.com>
2024-10-08 19:00:53 -0500

    createPago y createPagoObject

--- Pagos/logic/logicPagos.py
+++ Pagos/logic/logicPagos.py
@@ -3,3 +3,19 @@ from ..models import Pago
 def getPagos():
     pagos = Pago.objects.all()
     return pagos
+
+def createPago(form):
+    pago = form.save()
+    pago.save()
+    return ()
+
+def createPagoObject(monto,tipo,fecha,id,factura):
+    pago = Pago()
+    pago.monto = monto
+    pago.tipo = tipo
+    pago.fecha = fecha
+    pago.id = id
+    pago.factura = factura
+    pago.save()
+    return()
+    
