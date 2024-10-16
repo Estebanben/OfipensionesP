5b9956eb1eb94ce27a6191a17cb1659a496bd881
Esteban Benavides <estebanben717@gmail.com>
2024-10-16 15:12:55 -0500

    modelos creados

--- Colegios/models.py
+++ Colegios/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Colegio(models.Model):
+    id = models.AutoField(primary_key=True)
+    ubicacion = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    cursos = models.IntegerField()
+    
+class Curso(models.Model):
+    id = models.AutoField(primary_key=True)
+    anio = models.CharField(max_length=100)
+    fecha = models.DateField()
+    nombre = models.CharField(max_length=100)
+    colegio = models.ForeignKey(Colegio,on_delete=models.CASCADE)
--- Facturas/models.py
+++ Facturas/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 from Usuarios.models import Estudiante
+from Facturas.models import CronogramaDePago
+from Colegios.models import Curso,Colegio
 
 class Factura(models.Model):
     id = models.AutoField(primary_key=True)
@@ -11,6 +13,7 @@ class Factura(models.Model):
     abonado = models.IntegerField()
     intereses = models.IntegerField()
     estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    cronograma = models.ForeignKey(CronogramaDePago, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Factura ID: {self.id} | Estudiante: {self.estudiante.nombre} | Fecha de Inicio: {self.fechaIni} | Valor: {self.precio} | Fecha de Vencimiento: {self.fechaVen}'
@@ -30,3 +33,18 @@ class Producto(models.Model):
     
     def __str__(self):
         return self.nombre
+
+class CronogramaDePago(models.Model):
+    id = models.AutoField(primary_key=True)
+    tipo = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    cursos = models.ManyToManyField(Curso)
+    colegio = models.ForeignKey(Colegio, on_delete=models.CASCASDE)
+
+
+class DetalleCronograma(models.Model):
+    id = models.AutoField(primary_key=True)
+    valor = models.IntegerField(max_length=100)
+    fechaCausacion = models.DateField()
+    fechaLimite = models.DateField()
+    cronograma = models.ForeignKey(CronogramaDePago, on_delete=models.CASCADE)
