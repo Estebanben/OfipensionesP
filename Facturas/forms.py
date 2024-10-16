df8ec641c9c8d1cac09c10c625f4e82c8d23d144
Esteban Benavides <estebanben717@gmail.com>
2024-10-16 17:42:57 -0500

    cambios texto html y forms

--- Facturas/forms.py
+++ Facturas/forms.py
@@ -2,8 +2,8 @@
 from django import forms
 
 class FacturaForm(forms.Form):
-    start_date = forms.DateField(label='Fecha de Inicio', widget=forms.DateInput(attrs={'type': 'date'}))
-    end_date = forms.DateField(label='Fecha de Vencimiento', widget=forms.DateInput(attrs={'type': 'date'}))
+    start_date = forms.DateField(label='Fecha inicial', widget=forms.DateInput(attrs={'type': 'date'}))
+    end_date = forms.DateField(label='Fecha final', widget=forms.DateInput(attrs={'type': 'date'}))
     colegio_id = forms.IntegerField(label='ID del Colegio')
     
     
