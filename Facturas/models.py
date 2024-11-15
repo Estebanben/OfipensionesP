8eb4d32228761f364e17d11f790978889efd42bc
Your Name <you@example.com>
2024-11-14 21:09:14 -0500

    otro cambio (la mala para miguel)

--- Facturas/models.py
+++ Facturas/models.py
@@ -21,7 +21,7 @@ class Factura(models.Model):
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     valor = models.IntegerField()
-    dscripcion = models.CharField(max_length=1000)
+    descripcion = models.CharField(max_length=1000)
     
     def __str__(self):
         return self.nombre
