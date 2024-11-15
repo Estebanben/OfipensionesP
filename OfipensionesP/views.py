ec803f00ccbd20a6d0dfea14b7b240ce08077dc0
Nicolas Arango <n.arangor@uniandes.edu.co>
2024-11-15 17:05:56 -0500

    home

--- OfipensionesP/views.py
+++ OfipensionesP/views.py
@@ -1,4 +1,5 @@
 from django .http  import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    return HttpResponse ("Hello world! Django views")
+def index(request):
+    return render(request, 'index.html')
