586fc1e2aadaed623155df928fdd8cdeb937d0dd
Your Name <you@example.com>
2024-11-15 14:32:37 -0500

    manejo de errores

--- Facturas/models.py
+++ Facturas/models.py
@@ -25,3 +25,4 @@ class Producto(models.Model):
     
     def __str__(self):
         return self.nombre
+
--- OfipensionesP/pruebas_views.py
+++ OfipensionesP/pruebas_views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from Facturas.models import Producto  
+from Facturas.models import Producto
+
+PALABRAS_RESERVADAS = ['DELETE', 'DROP', 'INSERT', 'UPDATE', '--', ';', 'SELECT']
 
 def prueba_inyeccion_sql(request):
     if request.method == 'POST':
         nombre = request.POST.get('nombre')
         precio = request.POST.get('precio')
         descripcion = request.POST.get('descripcion')
+
+        if not nombre or not precio or not descripcion:
+            return HttpResponse('Error: Faltan campos por rellenar.', status=400)
+
+        for palabra in PALABRAS_RESERVADAS:
+            if any(palabra in campo for campo in [nombre, precio, descripcion]):
+                return HttpResponse('Error: Entrada no permitida debido a palabras reservadas.', status=400)
+
         try:
             Producto.objects.create(nombre=nombre, valor=precio, dscripcion=descripcion)
             return HttpResponse('Producto creado exitosamente.')
         except Exception as e:
             return HttpResponse(f'Error al crear el producto: {str(e)}')
+
     return render(request, 'formulario.html')
