4f14a6d91435276a3d6688790695c052f936d329
Nicolas Arango <n.arangor@uniandes.edu.co>
2024-11-15 17:14:39 -0500

    estudiantes

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('hijos/',views.hijos_del_padre,name='hijos_del_padre')
+    path('estudiantes/',views.estudiantes_list,name='estudiantes_list')
 ]
--- Usuarios/views.py
+++ Usuarios/views.py
@@ -1,21 +1,38 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
-from .models import Estudiante, Padre
+from .logic.logicEstudiantes import getEstudiantes
+from django.core import serializers
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from Autenticacion.auth0backend import getRole
 
-def hijos_del_padre(request):
-    # Verifica si el usuario está autenticado
-    if request.user.is_authenticated:
-        try:
-            # Obtiene el padre a partir del usuario autenticado
-            padre = Padre.objects.get(correo=request.user.email)
-            # Obtiene los hijos asociados a este padre
-            hijos = Estudiante.objects.filter(padre=padre)
-        except Padre.DoesNotExist:
-            # Si el padre no existe, retorna una lista vacía
-            hijos = []
 
-        # Renderiza los datos de los hijos en el template
-        return render(request, 'hijos_del_padre.html', {'hijos': hijos})
+# Create your views here.
+@login_required
+def estudiantes_list(request):
+    role = getRole(request)
+    if role == 'Padre':
+        estudiantes = getEstudiantes(request)
+        context = {'estudiantes_list': estudiantes}
+        return render(request, 'Estudiante/estudiantes_list.html', context)
     else:
-        # Redirige a la página de inicio de sesión si no está autenticado
-        return render(request, 'hijos_del_padre.html', {'hijos': hijos})
+        return HttpResponse('No tienes permisos para ver esta pagina')
+    
+# def hijos_del_padre(request):
+#     # Verifica si el usuario está autenticado
+#     if request.user.is_authenticated:
+#         try:
+#             # Obtiene el padre a partir del usuario autenticado
+#             padre = Padre.objects.get(correo=request.user.email)
+#             # Obtiene los hijos asociados a este padre
+#             hijos = Estudiante.objects.filter(padre=padre)
+#         except Padre.DoesNotExist:
+#             # Si el padre no existe, retorna una lista vacía
+#             hijos = []
+
+#         # Renderiza los datos de los hijos en el template
+#         return render(request, 'hijos_del_padre.html', {'hijos': hijos})
+#     else:
+#         # Redirige a la página de inicio de sesión si no está autenticado
+#         return render(request, 'hijos_del_padre.html', {'hijos': hijos})
+    
+        
